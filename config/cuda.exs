4de93c29bfb8f74f3d999c667d17fe36e1e6991e
Jia Wang <wj2016@gmail.com>
2024-02-04 23:13:08 +0100

    Try to solve OOM issue

--- config/cuda.exs
+++ config/cuda.exs
@@ -1,11 +1,13 @@
 import Config
 
-# config :nx, :default_backend, EXLA.Backend
-config :nx, :default_backend, {EXLA.Backend, client: :cuda}
-config :nx, :default_defn_options, compiler: EXLA
+# https://elixirforum.com/t/livebook-cuda-12-2-xla-out-of-memory-at-11006mib/58054/2
+# Load the parameters into the CPU
+config :nx, :default_backend, {EXLA.Backend, client: :host}
+# Enable lazy transfers in serving defn options
+config :nx, :default_defn_options, compiler: EXLA, lazy_transfers: :always
 
 # https://hexdocs.pm/exla/EXLA.html#module-clients
-config :exla, :default_client, :cuda
+# config :exla, :default_client, :cuda
 
 config :exla, :clients,
   host: [platform: :host],
